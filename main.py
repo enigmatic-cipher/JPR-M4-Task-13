4d794e707cb992c3937af1aa9867d77fa66bd19a
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-02-13 18:00:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+Given a string representing date in "dd/mm/yyyy" format convert it to dd-Mon-yyyy format.
+Input-> "12/03/2016"
+Output-> 12-Mar-2016
+"""
+
+dt = ("Enter Date in dd/mm/yyyy format: ")
